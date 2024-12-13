7f4efead0891fa5e87e9ec2f8a99050122072911
SamantaSoumya <soumyasamanta354@gmail.com>
2024-12-13 22:17:42 +0530

    initial commit

---
+++ src/main/java/com/CELA/CELA/CelaApplication.java
@@ -0,0 +1,13 @@
+package com.CELA.CELA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelaApplication.class, args);
+	}
+
+}
