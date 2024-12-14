a751a8a2de10da29c3cd5ca7ab4b0b0fcc1ef009
SamantaSoumya <soumyasamanta354@gmail.com>
2024-12-14 23:15:17 +0530

    add properties for log and others

--- src/main/java/com/CELA/CELA/CelaApplication.java
+++ src/main/java/com/CELA/CELA/CelaApplication.java
@@ -1,13 +1,20 @@
 package com.CELA.CELA;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CelaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CelaApplication.class, args);
-	}
+    // Create a logger instance
+    private static final Logger logger = LogManager.getLogger(CelaApplication.class);
 
+    public static void main(String[] args) {
+        logger.info("main method started");
+        SpringApplication.run(CelaApplication.class, args);
+        logger.debug("Thank you everything is all right");
+    }
 }
